0bedc94f9eafc5de72c8aad31ec605ed520024d3
unknown <heitormorak@gmail.com>
2022-10-26 22:33:36 -0300

    trying to split

--- teste/src/teste/Finder.java
+++ teste/src/teste/Finder.java
@@ -20,37 +20,41 @@ public class Finder {
 		 * 
 		 * String linha = null;
 		 * 
-		 * while ((linha = Buffer.readLine()) != null) { String arr[] =
-		 * linha.slipt(" "); for(int i = 0; i< arr.length; i++) { if
-		 * (arr[i].trim().equals(uf)) { System.out.println(linha); } } } } catch
+		 * while ((linha = Buffer.readLine()) != null) { 
+		 * String arr[] = linha.slipt(" "); 
+		 * for(int i = 0; i< arr.length; i++) { 
+		 * if (arr[i].trim().equals(uf)) { 
+		 * System.out.println(linha);
+		 * } } } 
+		 * } catch
 		 * (Exception e) { System.out.print("" + e); }
 		 */
 		
 		//retorna nome do estado e a regiao 1 2 3
 		
-		Scanner leitor = new Scanner(System.in);
-        System.out.println("Digite o nome do arquivo texto: ");
-        String nomeArq = leitor.nextLine();
+		/*
+		 * Scanner leitor = new Scanner(System.in);
+		 * System.out.println("Digite o nome do arquivo texto: "); String nomeArq =
+		 * leitor.nextLine();
+		 */
         
         Scanner uf = new Scanner(System.in);
         System.out.println("Digite a UF: ");
         String nomeUf = uf.nextLine();
         
-
-        System.out.println("Conteúdo do arquivo texto:");
         try {
-            FileReader arq = new FileReader(nomeArq);
-            BufferedReader lerArq = new BufferedReader(arq);
-
+            FileReader arq = new FileReader("UF.csv");
+            BufferedReader lerArq = new BufferedReader(arq);  
+            
             String linha = lerArq.readLine();
-            while(linha != null){
-                if (linha.contains(nomeUf)) {
-            	System.out.println(linha);
-            	System.out.println(nomeUf);
-                }
-                linha = lerArq.readLine(); // lê da segunda até a última linha
+            while((linha = lerArq.readLine()) != null){
+            	String arr[] = linha.split(","); 
+            	for(int i = 0; i< arr.length; i++) { 
+            		if (arr[i].trim().equals(uf)) { 
+            			System.out.println(linha);            	
+            		}
+            	}  
             }
-            arq.close();
         }catch (IOException e){
             System.err.printf("Erro na abertura do arquivo: ", e.getMessage());
         }
