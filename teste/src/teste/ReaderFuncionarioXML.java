7b7ce0cfbafba1df717f0dc94db98ac846c140b2
unknown <heitormorak@gmail.com>
2022-11-09 22:05:07 -0300

    feat: starting xml reader

--- teste/src/teste/ReaderFuncionarioXML.java
+++ teste/src/teste/ReaderFuncionarioXML.java
@@ -1,11 +1,15 @@
 package teste;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class ReaderFuncionarioXML {
 	
@@ -16,13 +20,25 @@ public class ReaderFuncionarioXML {
 		String nomeArq = leitor.nextLine();
 		
 		try {
-			FileReader arq = new FileReader(nomeArq + ".json");
-			BufferedReader lerArq = new BufferedReader(arq);
+			XStream xstream = new XStream(new DomDriver());
 			
-			String linha = lerArq.readLine();
+			//xstream.alias("produto", Funcionario.class);
+			FileInputStream inputStream = new FileInputStream(new File(nomeArq + ".xml"));
+			Funcionario fun = (Funcionario) xstream.fromXML(inputStream);
+			//List<Produto> produtos = (List<Produto>) xstream.fromXML(inputStream);
 			
-			Gson objGson = new Gson();			
-			Funcionario fun = objGson.fromJson(linha, Funcionario.class);
+			
+			/*
+			 * FileReader arq = new FileReader(nomeArq + ".xml"); 
+			 * BufferedReader lerArq =
+			 * new BufferedReader(arq);
+			 * 
+			 * String linha = lerArq.readLine();
+			 * 
+			 * Gson objGson = new Gson(); 
+			 * Funcionario fun = objGson.fromJson(linha,
+			 * Funcionario.class);
+			 */
 			System.out.println("Dados do objeto originados do JSON:");
 			System.out.println ("CPF: "+ fun.getCpf());
 			System.out.println ("Nome: "+ fun.getNome());
@@ -30,7 +46,7 @@ public class ReaderFuncionarioXML {
 			System.out.println ("Cargo: "+ fun.getCargo());
 			System.out.println ("Habilidades: "+ fun.getHabilidades());
 			
-			arq.close();			
+			//arq.close();			
 		} catch (IOException e){
 			System.err.printf("Nenhum arquivo encontrado com o CPF informado", e.getMessage());
 		}
